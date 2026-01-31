7e3ef2b155e74cc6004e9a59a99c6f021ac3e2c9
haroon6268 <136369742+haroon6268@users.noreply.github.com>
2026-01-31 09:43:07 -0500

    added shit

--- foodfriend/app/_layout.tsx
+++ foodfriend/app/_layout.tsx
@@ -2,8 +2,14 @@ import { AppProvider } from "@/context/AppContext";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+	useEffect(() => {
+		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+	}, []);
+
 	return (
 		<ClerkProvider tokenCache={tokenCache}>
 			<AppProvider>
